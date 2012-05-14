b77f30c6423de2c9a7a04e5d457d6eae8e895a9e
Rodrigo Setti <rodrigosetti@gmail.com>
2012-05-14 12:28:00 -0300

    created program wireframes

---
+++ lplot.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python
+# coding: utf-8
+
+def lines_2d(l_string, theta, absolute_positions=True):
+    """
+    Return a set of 2-tuples of 2D coordinates (2-tuples) that represent
+    a plot of a given l_string
+    """
+    pass
+
+def lines_3d(l_string, theta, omega, abolute_positions=True):
+    """
+    Return a set of 2-tuples of 3D coordinates (3-tuples) that represent
+    a plot of a given l_string
+    """
+    pass
+
+if __name__ == "__main__":
+
+    # Input: A L-string from (stdin)
+    # Output: An output file that describes an image
+
+    # Command line parameters
+    # Theta and Omega (optional numeric parameters)
+    # --3d and --2d (disjoint boolean parameters)
+    # Output type (string, one of: png,jpeg,bmp,povray)
+
+    pass
+
+
---
+++ lsystem.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+# coding: utf-8
+
+
+def expand(grammar, axiom, times):
+    """
+    Get the l_string of the expanded axiom string N times for the given
+    grammar rules
+    """
+    pass
+
+if __name__ == "__main__":
+
+    # Input: L-System grammar, axiom and apply times
+    # Ouput: Generated L-String (stdout)
+
+    # Command line parameters:
+    # Axiom (one string parameter)
+    # Rules (zero or more string parameters in the form: .:.*)
+    # Times (optional integer > 0 parameter)
+
+    pass

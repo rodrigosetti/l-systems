5c44959cf481f2d9c45092758468503e963fb907
Rodrigo Setti <rodrigosetti@gmail.com>
2012-05-14 19:01:10 -0300

    optimize expand

--- lsystem.py
+++ lsystem.py
@@ -3,22 +3,13 @@
 
 __all__ = ['expand']
 
-import re
-
 def expand(grammar, axiom, times=0):
     """
     Get the l_string of the expanded axiom string N times for the given
     grammar rules
     """
     for n in xrange(times):
-        l_string = []
-        for char in axiom:
-            if char in grammar:
-                l_string.append( grammar[char])
-            else:
-                l_string.append( char )
-
-        axiom = ''.join( l_string )
+        axiom = ''.join( grammar[c] if c in grammar else c for c in axiom )
 
     return axiom
 

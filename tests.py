ca9e31af1284fb43ca4fbc3a218b8d1f484aa86a
Rodrigo Setti <rodrigosetti@gmail.com>
2012-05-16 13:55:28 -0300

    adds tests for commands

--- tests.py
+++ tests.py
@@ -1,11 +1,10 @@
 #! /usr/bin/env python
 # coding: utf-8
 
-from lsystem import expand
-
+from lsystem import *
 import unittest
 
-class TestLSystemExpand(unittest.TestCase):
+class TestExpand(unittest.TestCase):
 
     def test_empty_grammar_should_not_apply_any_transformation(self):
 
@@ -44,6 +43,60 @@ class TestLSystemExpand(unittest.TestCase):
         times = 0
 
         self.assertEquals(axiom, ''.join(expand(grammar, axiom, times)))
+
+    def test_non_terminal_chars_should_be_left_out(self):
+
+        grammar = { 'f': 'AA', 'A': 'Bf', '[': 'AB'}
+        axiom = 'fA-[B'
+        times = 3
+
+        #0: f         A        - [    B
+        #1: A    A    B  f     - A    B B
+        #2: B f  B f  B  A  A  - B f  B B
+        #3: B AA B AA B  Bf Bf - B AA B B
+
+        expected = 'ff-'
+
+        self.assertEquals(expected, ''.join(expand(grammar, axiom, times)))
+
+class TestCommands(unittest.TestCase):
+
+    def test_commands_should_return_a_tuple_for_each_command(self):
+
+        l_string = '<-f[+]>'
+        length = 10
+        delta_theta = 25
+        expected = [(TURN_Z, -25), (TURN_Y, -25), (MOVE, 10), ('[', None), (TURN_Y, 25), (']', None), (TURN_Z, 25)]
+
+        self.assertEquals( expected, list(commands(l_string, length, delta_theta)) )
+
+    def test_commands_should_accumulate_moves(self):
+
+        l_string = 'ffffffffff'
+        length = 10
+        delta_theta = 25
+        expected = [(MOVE, 100)]
+
+        self.assertEquals( expected, list(commands(l_string, length, delta_theta)) )
+
+    def test_commands_should_accumulate_turns(self):
+
+        l_string = '+-++-+<<<><<>>'
+        length = 10
+        delta_theta = 25
+        expected = [(TURN_Y, 50), (TURN_Z, -50)]
+
+        self.assertEquals( expected, list(commands(l_string, length, delta_theta)) )
+
+    def test_commands_should_drop_zero_turns(self):
+
+        l_string = '+-++-+--<<<><><>>>'
+        length = 10
+        delta_theta = 25
+        expected = []
+
+        self.assertEquals( expected, list(commands(l_string, length, delta_theta)) )
+
 
 
 if __name__ == "__main__":

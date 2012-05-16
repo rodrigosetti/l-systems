b372a88372312f033a93942f83a9562f3077190b
Rodrigo Setti <rodrigosetti@gmail.com>
2012-05-16 13:34:39 -0300

    tests fixed

--- tests.py
+++ tests.py
@@ -2,7 +2,6 @@
 # coding: utf-8
 
 from lsystem import expand
-from lplot import lines_2d, lines_3d
 
 import unittest
 
@@ -11,50 +10,41 @@ class TestLSystemExpand(unittest.TestCase):
     def test_empty_grammar_should_not_apply_any_transformation(self):
 
         grammar = {}
-        axiom = 'AXIOM'
+        axiom = 'f+-[]'
 
-        self.assertEquals(axiom, expand(grammar, axiom))
+        self.assertEquals(axiom, ''.join(expand(grammar, axiom)))
 
     def test_expand_one_time_should_replace_matches_by_values_once(self):
 
-        grammar = { 'A': 'XX', 'X': 'MM', 'O': 'XM'}
-        axiom = 'AXIOM'
-        expected = 'XXMMIXMM'
+        grammar = { 'f': '++', '+': ']]', '[': '+]'}
+        axiom = 'f+-[]'
+        expected = '++]]-+]]'
 
-        self.assertEquals(expected, expand(grammar, axiom, 1))
+        self.assertEquals(expected, ''.join(expand(grammar, axiom, 1)))
 
     def test_expand_several_times_should_replace_matches_by_values_N_times(self):
 
-        grammar = { 'A': 'XX', 'X': 'MA', 'O': 'XM'}
-        axiom = 'AXIOM'
+        grammar = { 'f': '++', '+': ']f', '[': '+]'}
+        axiom = 'f+-[]'
         times = 3
 
-        #0: A         X        I O    M
-        #1: X    X    M  A     I X    M M
-        #2: M A  M A  M  X  X  I M A  M M
-        #3: M XX M XX M  MA MA I M XX M M
+        #0: f         +        - [    ]
+        #1: +    +    ]  f     - +    ] ]
+        #2: ] f  ] f  ]  +  +  - ] f  ] ]
+        #3: ] ++ ] ++ ]  ]f ]f - ] ++ ] ]
 
-        expected = 'MXXMXXMMAMAIMXXMM'
+        expected = ']++]++]]f]f-]++]]'
 
-        self.assertEquals(expected, expand(grammar, axiom, times))
+        self.assertEquals(expected, ''.join(expand(grammar, axiom, times)))
 
     def test_zero_times_should_always_return_axiom(self):
 
-        grammar = { 'A': 'XX', 'X': 'MA', 'O': 'XM'}
-        axiom = 'AXIOM'
+        grammar = { 'f': '++', '+': ']f', '[': '+]'}
+        axiom = 'f+-[]'
         times = 0
 
-        self.assertEquals(axiom, expand(grammar, axiom, times))
+        self.assertEquals(axiom, ''.join(expand(grammar, axiom, times)))
 
-class TestLines2D(unittest.TestCase):
-
-    def test_single_f_should_return_a_single_line_with_length(self):
-
-        l_string = 'f'
-        length = 100
-        expected = set( (0,100) )
-
-        self.assertEquals( expected, lines_2d(l_string, length, 0, True) )
 
 if __name__ == "__main__":
     unittest.main()
